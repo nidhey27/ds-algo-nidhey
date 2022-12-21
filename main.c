ad5d0c30d33bf001819331c5213a72d89e386845
Nidhey Nitin Indurkar <nidhey60@gmail.com>
2022-12-21 07:33:57 +0000

    pointers

--- main.c
+++ main.c
@@ -7,33 +7,35 @@
 
     5. DS
     6. Time & Space Complexity
-    7. Data Types - Built In & User Defined (int float char - struct union arrays)
-    8. Algorithms 
+    7. Data Types - Built In & User Defined (int float char - struct union
+   arrays)
+    8. Algorithms
 */
 
-#include<stdio.h>
-void swap_val(int x, int y)
-{
-    int temp;
-    temp=x;
-    x=y;
-    y=temp;
-    printf("%d %d",x,y);
+#include <stdio.h>
+void swap_val(int x, int y) {
+  int temp;
+  temp = x;
+  x = y;
+  y = temp;
+  printf("Inside Val Call %d %d\n", x, y);
 }
-void swap_ref(int *x, int *y)
-{
-    int temp;
-    temp=*x;
-    *x=*y;
-    *y=temp;
-    printf("%d %d",*x,*y);
+void swap_ref(int *x, int *y) {
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
 }
 
-int main(){
-    int a = 5;
-    int b = 10;
+int main() {
+  int a = 5;
+  int b = 10;
 
-    swap_ref(&a, &b);
-    swap_val(a, b);
+  printf("Before Ref Call %d %d\n", a, b);
+  swap_ref(&a, &b);
+  printf("After Ref Call %d %d\n", a, b);
+
+  printf("Before Val Call %d %d\n", a, b);
+  swap_val(a, b);
+  printf("After Val Call %d %d\n", a, b);
 }
-
